f19baa66c273babe5100d4cefc08598079731c71
Lucas Hogg <73367876+lucashogg@users.noreply.github.com>
2024-03-07 17:19:56 -0500

    Add initial js

---
+++ script.js
@@ -0,0 +1,15 @@
+// Array of books
+const myLibrary = [];
+// General book constructor
+class Book {
+    constructor(title, author, pages) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = false;
+    }
+}
+// Add books to library function
+function addBookToLibrary () {
+    // something
+}
