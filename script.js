88a87414c66a69494f5a5fa7f74e1658ef5ce457
Lucas Hogg <73367876+lucashogg@users.noreply.github.com>
2024-03-08 08:07:30 -0500

    Add book form capture function

--- script.js
+++ script.js
@@ -11,5 +11,22 @@ class Book {
 }
 // Add books to library function
 function addBookToLibrary () {
-    // something
+    const title = document.querySelector('#title');
+    const author = document.querySelector('#author');
+    const pages = document.querySelector('#pages');
+    const read = document.querySelector('#read').checked;
+
+    const newBook = new Book(title.value, author.value, pages.value);
+    if (read) {
+        newBook.read = read;
+    };
+
+    myLibrary.push(newBook);
 }
+// Listen for book submit
+const addBookBtn = document.querySelector('#add-book-button');
+
+addBookBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    addBookToLibrary()
+})
