782ddad50484d46b015238fa03991bb2f92ccec9
Lucas Hogg <73367876+lucashogg@users.noreply.github.com>
2024-03-09 14:03:51 -0500

    Add custom input required function

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ class Book {
     }
 }
 // Add book to library function
-function addBookToLibrary () {
+function addBookToLibrary() {
     const title = document.querySelector('#title');
     const author = document.querySelector('#author');
     const pages = document.querySelector('#pages');
@@ -27,10 +27,39 @@ function addBookToLibrary () {
     const addBookForm = document.querySelector('.add-book-form');
     addBookForm.reset();
 }
+
+// Insert book info into card
+function addBookCard() {
+    for (const book of myLibrary) {
+        for (const key in book) {
+            // console.log(book[key]);
+        }
+    }
+}
+
+// Check if form requirements met
+function formReq() {
+    const formInputs = document.querySelectorAll('.add-book-form input');
+
+    for (let i = 0; i < formInputs.length; i++) {
+        if (formInputs[i].type === 'checkbox') {
+            continue;
+        } else if (!formInputs[i].value) {
+            console.log('value req');
+            return false;
+        }
+    }
+    
+    return true;
+}
+
 // Listen for book submit
 const addBookBtn = document.querySelector('#add-book-button');
 
 addBookBtn.addEventListener('click', (e) => {
-    e.preventDefault()
-    addBookToLibrary()
+    e.preventDefault();
+    if (formReq()) {
+        addBookToLibrary();
+        addBookCard();
+    }
 })
